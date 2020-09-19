30f74b55eb20b540c629c2e9b3381fc09279b4c1
Zevi Liao <jointjm@gmail.com>
2020-09-20 05:41:27 +0800

    first commit

---
+++ src/hello/hello.js
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+import './hello.scoped.css'
+
+const hello = () => {
+    return (
+        <h1 className="font-color">hello, zevi</h1>
+    )
+}
+
+export default hello
