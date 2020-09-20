bb3635e2033a009eae8c06398c62d2e61045bfe3
Zevi Liao <jointjm@gmail.com>
2020-09-20 15:18:53 +0800

    update

--- src/emodemo/emComp.js
+++ src/emodemo/emComp.js
@@ -1,31 +1,30 @@
-/** @jsx jsx */
+
 import React from 'react'
-import { jsx, css } from '@emotion/core'
+import { ClassNames } from '@emotion/core'
 
-const style = css`
-  color: hotpink;
-`
-
-const SomeComponent = ({ children }) => (
-    <div css={style}>
-        Some hotpink text.
-        {children}
-    </div>
+// this might be a component from npm that accepts a wrapperClassName prop
+let SomeComponent = props => (
+  <div className={props.wrapperClassName}>
+    in the wrapper!
+    <div className={props.className}>{props.children}</div>
+  </div>
 )
 
-const anotherStyle = css({
-    textDecoration: 'underline'
-})
-
-const AnotherComponent = () => (
-    <div css={anotherStyle}>Some text with an underline.</div>
-)
 
 const emComp = () => {
     return (
-        <SomeComponent>
-            <AnotherComponent />
-        </SomeComponent>
+        <ClassNames>
+        {({ css, cx }) => (
+          <SomeComponent
+            wrapperClassName={css({ color: 'green' })}
+            className={css`
+              color: hotpink;
+            `}
+          >
+            from children!!
+          </SomeComponent>
+        )}
+      </ClassNames>
     )
 }
 
